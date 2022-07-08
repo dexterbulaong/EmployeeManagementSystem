39334c24b7f90994d17b78748a37d132ffd793dc
dexterbulaong <dexter.j1.bulaong@gmail.com>
2022-07-07 17:27:36 -0800

    first commit into new project

---
+++ src/test/java/com/gihub/dexterbulaong/EmployeeManagementSystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihub.dexterbulaong.EmployeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
